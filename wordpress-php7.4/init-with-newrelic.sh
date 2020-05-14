84070af47fabcc50b9bab58fc2a0ca53a0825cfb
Arif Widi Nugroho <arif@sainsmograf.com>
2020-05-15 01:24:33 +0700

    added newrelic support

---
+++ wordpress-php7.4/init-with-newrelic.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ "$NEWRELIC_INSTALL" = 'YES' ]; then
+    echo "enabling newrelic"
+    curl -L $NEWRELIC_PHP_AGENT_URL | tar -C /tmp -zx
+    
+    export NR_INSTALL_USE_CP_NOT_LN=1
+    export NR_INSTALL_SILENT=1
+    /tmp/newrelic-php5-*/newrelic-install install
+    rm -rf /tmp/newrelic-php5-* /tmp/nrinstall*
+    sed -i \
+          -e 's/"REPLACE_WITH_REAL_KEY"/"${NEWRELIC_LICENSE_KEY}"/' \
+          -e 's/newrelic.appname = "PHP Application"/newrelic.appname = "${NEWRELIC_APP_NAME}"/' \
+          -e 's/;newrelic.daemon.app_connect_timeout =.*/newrelic.daemon.app_connect_timeout=15s/' \
+          -e 's/;newrelic.daemon.start_timeout =.*/newrelic.daemon.start_timeout=5s/' \
+          /usr/local/etc/php/conf.d/newrelic.ini
+fi
+
+exec docker-entrypoint.sh "$@"
