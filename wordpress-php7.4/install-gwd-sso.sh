9ffe65e5f114a348c3da277e6d62e969dc588c76
Arif Widi Nugroho <arif@sainsmograf.com>
2022-06-03 01:33:14 +0700

    updated sso installation script

--- wordpress-php7.4/install-gwd-sso.sh
+++ wordpress-php7.4/install-gwd-sso.sh
@@ -2,9 +2,9 @@
 set -e
 
 if [ -z "${OPENID_CONNECT_CLIENT_ID}" ]; then
-    echo 'skip installing openid connect client'
+    echo 'No OpenID Connect client id defined. OpenID Connect client will not be installed.'
 else
-    echo 'installing openid connect client in 1 minutes...'
+    echo 'installing OpenID Connect client in 1 minutes...'
     sleep 1m
 
     if [ ! -d "/var/www/html/wp-content/mu-plugins" ]; then
@@ -12,14 +12,16 @@ else
         chown "33:33" -R /var/www/html/wp-content/mu-plugins
     fi
 
-    if [ ! -d "/var/www/html/wp-content/mu-plugins/gwd-openid-connect-admin-login" ]; then
-        cp -r /var/mu-plugins/gwd-openid-connect-admin-login /var/www/html/wp-content/mu-plugins/
-        chown "33:33" -R /var/www/html/wp-content/mu-plugins
+    if [ -d "/var/www/html/wp-content/mu-plugins/gwd-openid-connect-admin-login" ]; then
+        rm -rf /var/www/html/wp-content/mu-plugins/gwd-openid-connect-admin-login 
     fi
 
-    if [ ! -f /var/www/html/wp-content/mu-plugins/gwd-openid-connect-admin-login.php ]; then
-        cp /var/mu-plugins/gwd-openid-connect-admin-login.php /var/www/html/wp-content/mu-plugins/
-        chown "33:33" -R /var/www/html/wp-content/mu-plugins
+    if [ -f /var/www/html/wp-content/mu-plugins/gwd-openid-connect-admin-login.php ]; then
+        rm /var/www/html/wp-content/mu-plugins/gwd-openid-connect-admin-login.php
     fi
-    echo 'done'
+
+    cp -r /var/mu-plugins/gwd-openid-connect-admin-login /var/www/html/wp-content/mu-plugins/
+    cp /var/mu-plugins/gwd-openid-connect-admin-login.php /var/www/html/wp-content/mu-plugins/
+    chown "33:33" -R /var/www/html/wp-content/mu-plugins
+    echo 'OpenID Connect installed'
 fi
